3ca1141ded74641c75e7dd4cf646d2313237e451
Sahil Ghorpade <sahilpg27@gmail.com>
2025-07-04 19:19:52 +0530

    Frontend set up done and working

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Meal Saver</h1>
+      <p>Welcome to the Meal Saver System</p>
+    </div>
+  );
+}
+
+export default App;
